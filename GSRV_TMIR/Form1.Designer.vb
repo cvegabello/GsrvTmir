b57a7b02b77764f2dd626111b553694308c3d85e
cvegabello <carlosvegabello@gmail.com>
2021-05-09 20:29:47 -0400

    First commit

---
+++ GSRV_TMIR/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.okBtn = New System.Windows.Forms.Button
+        Me.cancelBtn = New System.Windows.Forms.Button
+        Me.stopBtn = New System.Windows.Forms.Button
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'okBtn
+        '
+        Me.okBtn.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.okBtn.Image = CType(resources.GetObject("okBtn.Image"), System.Drawing.Image)
+        Me.okBtn.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.okBtn.Location = New System.Drawing.Point(215, 83)
+        Me.okBtn.Name = "okBtn"
+        Me.okBtn.Size = New System.Drawing.Size(84, 46)
+        Me.okBtn.TabIndex = 21
+        Me.okBtn.Text = "&OK"
+        Me.okBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.okBtn.UseVisualStyleBackColor = True
+        '
+        'cancelBtn
+        '
+        Me.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.cancelBtn.Image = CType(resources.GetObject("cancelBtn.Image"), System.Drawing.Image)
+        Me.cancelBtn.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.cancelBtn.Location = New System.Drawing.Point(305, 83)
+        Me.cancelBtn.Name = "cancelBtn"
+        Me.cancelBtn.Size = New System.Drawing.Size(84, 46)
+        Me.cancelBtn.TabIndex = 22
+        Me.cancelBtn.Text = "&Cancel"
+        Me.cancelBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.cancelBtn.UseVisualStyleBackColor = True
+        '
+        'stopBtn
+        '
+        Me.stopBtn.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.stopBtn.Location = New System.Drawing.Point(23, 83)
+        Me.stopBtn.Name = "stopBtn"
+        Me.stopBtn.Size = New System.Drawing.Size(367, 46)
+        Me.stopBtn.TabIndex = 20
+        Me.stopBtn.Text = "Stop"
+        Me.stopBtn.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(267, 17)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(123, 42)
+        Me.GroupBox2.TabIndex = 19
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "CDC"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(37, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "L"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 17)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(225, 42)
+        Me.GroupBox1.TabIndex = 18
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Date"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(9, 16)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(210, 20)
+        Me.DateTimePicker1.TabIndex = 1
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(23, 65)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(367, 12)
+        Me.ProgressBar1.TabIndex = 17
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 162)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 198)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 24
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 322)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.okBtn)
+        Me.Controls.Add(Me.cancelBtn)
+        Me.Controls.Add(Me.stopBtn)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents okBtn As System.Windows.Forms.Button
+    Friend WithEvents cancelBtn As System.Windows.Forms.Button
+    Friend WithEvents stopBtn As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ GSRV_TMIR/Form1.vb
@@ -0,0 +1,302 @@
+﻿Public Class Form1
+
+    Sub getHostModeNew(ByVal hostName As String, ByVal userName As String, ByVal password As String, ByVal hostNumberArray() As Integer, ByRef errCodeInt As Integer, ByRef errMessageStr As String)
+
+
+        Dim ssh As New Chilkat.Ssh()
+        Dim port As Integer
+        Dim channelNum, posInt, numInt, isIcludedInt, spareInt As Integer
+        Dim termType As String = "xterm"
+        Dim widthInChars As Integer = 80
+        Dim heightInChars As Integer = 24
+        Dim pixWidth As Integer = 0
+        Dim pixHeight As Integer = 0
+        Dim cmdOutputStr As String = ""
+        Dim msgError As String = ""
+        Dim n As Integer
+        Dim pollTimeoutMs As Integer = 2000
+
+        Dim success As Boolean = ssh.UnlockComponent("Anything for 30-day trial")
+
+        If (success <> True) Then
+            errCodeInt = -1
+            errMessageStr = "Error related to component license: " & ssh.LastErrorText
+            'MsgBox("Se presento un error #1. " + ssh.LastErrorText, MsgBoxStyle.OkOnly, "Error")
+            Exit Sub
+        End If
+
+        port = 22
+
+        success = ssh.Connect(hostName, port)
+        If (success <> True) Then
+            errCodeInt = -2
+            errMessageStr = "Error related to the conection: " & ssh.LastErrorText
+
+            'MsgBox("No Funciono Conneccion. " + ssh.LastErrorText, MsgBoxStyle.OkOnly, "NO funciono coneccion")
+            Exit Sub
+            'Else
+            '    MsgBox("EXITO Coneccion", MsgBoxStyle.OkOnly, "Funciono Conneccion")
+        End If
+
+
+        success = ssh.AuthenticatePw(userName, password)
+        If (success <> True) Then
+            errCodeInt = -3
+            errMessageStr = "Error related to the authentication: " & ssh.LastErrorText
+
+            'MsgBox("No Funciono Autenticacion. " + ssh.LastErrorText, MsgBoxStyle.OkOnly, "NO funciono Autenticacion")
+            Exit Sub
+            'Else
+            '    MsgBox("Funciono Autenticacion", MsgBoxStyle.OkOnly, "Funciono Autenticacion. ")
+        End If
+
+        channelNum = ssh.OpenSessionChannel()
+        If (channelNum < 0) Then
+            errCodeInt = -4
+            errMessageStr = "Error related to OpenSessionChannel: " & ssh.LastErrorText
+
+            'MsgBox("No Funciono Apertura Canal. " + ssh.LastErrorText, MsgBoxStyle.OkOnly, "NO funciono Apertura del canal")
+            Exit Sub
+            'Else
+            '    MsgBox("Funciono OpenSessionChannel", MsgBoxStyle.OkOnly, "Funciono OpenSessionChannel. ")
+
+        End If
+
+        success = ssh.SendReqPty(channelNum, termType, widthInChars, heightInChars, pixWidth, pixHeight)
+        If (success <> True) Then
+            errCodeInt = -5
+            errMessageStr = "Error related to SendReqPty: " & ssh.LastErrorText
+            'MsgBox("NO funciono Sendreqpty " + ssh.LastErrorText, MsgBoxStyle.OkOnly, "NO funciono Sendreqpty. ")
+            Exit Sub
+            'Else
+            '    MsgBox("Funciono SendReqPty", MsgBoxStyle.OkOnly, "Funciono SendReqPty. ")
+
+        End If
+
+        success = ssh.SendReqShell(channelNum)
+        If (success <> True) Then
+            errCodeInt = -6
+            errMessageStr = "Error related to SendReqShell: " & ssh.LastErrorText
+            'MsgBox("NO funciono SendReqShell " + ssh.LastErrorText, MsgBoxStyle.OkOnly, "NO funciono SendReqShell. ")
+            Exit Sub
+            'Else
+            '    MsgBox("Funciono SendReqShell", MsgBoxStyle.OkOnly, "Funciono SendReqShell. ")
+        End If
+
+
+        errCodeInt = SSHUntilMatch(ssh, "Enter Selection:", channelNum, pollTimeoutMs, cmdOutputStr, msgError)
+        If errCodeInt <> 0 Then
+            errCodeInt = -9
+            errMessageStr = ssh.LastErrorText
+            Exit Sub
+            'Else
+            '    MsgBox("FUNCIONO: ", MsgBoxStyle.OkOnly, "FUNCIONO.")
+
+        End If
+
+        errCodeInt = sentStringToSSHUntilMatch(ssh, "10", "Enter Selection:", channelNum, pollTimeoutMs, cmdOutputStr, msgError)
+        If errCodeInt <> 0 Then
+            errCodeInt = -9
+            errMessageStr = ssh.LastErrorText
+            Exit Sub
+            'Else
+            '    MsgBox("FUNCIONO: 10 ", MsgBoxStyle.OkOnly, "FUNCIONO.")
+        End If
+
+        errCodeInt = sentStringToSSHUntilMatch(ssh, "01", "Selection:", channelNum, pollTimeoutMs, cmdOutputStr, msgError)
+        If errCodeInt <> 0 Then
+            errCodeInt = -9
+            errMessageStr = ssh.LastErrorText
+            Exit Sub
+            'Else
+            '    MsgBox("FUNCIONO: 01 ", MsgBoxStyle.OkOnly, "FUNCIONO.")
+
+
+        End If
+
+        errCodeInt = sentStringToSSH(ssh, "PEERS", channelNum, pollTimeoutMs, cmdOutputStr, msgError)
+        If errCodeInt <> 0 Then
+            errCodeInt = -9
+            errMessageStr = ssh.LastErrorText
+            'MsgBox(msgError, MsgBoxStyle.OkOnly, "Error")
+            Exit Sub
+            'Else
+            '    RichTextBox1.Text = cmdOutputStr
+        End If
+
+        ssh.Disconnect()
+
+
+        posInt = InStr(1, cmdOutputStr, "live_idn:")
+        numInt = CInt(Mid(cmdOutputStr, posInt + 10, 2))
+        hostNumberArray(0) = numInt + 1
+
+        posInt = InStr(1, cmdOutputStr, "backup_idn:")
+        numInt = CInt(Mid(cmdOutputStr, posInt + 11, 2))
+        hostNumberArray(1) = numInt + 1
+
+        posInt = InStr(1, cmdOutputStr, "wait_for_spare:")
+        numInt = CInt(Mid(cmdOutputStr, posInt + 16, 2))
+        spareInt = numInt + 1
+
+        If ((spareInt = hostNumberArray(0)) Or (spareInt = hostNumberArray(1))) Then
+
+            For i = 1 To 5
+                isIcludedInt = Array.IndexOf(hostNumberArray, i)
+                If isIcludedInt < 0 Then
+                    hostNumberArray(2) = i
+                    Exit For
+                End If
+            Next
+        Else
+            hostNumberArray(2) = spareInt
+
+        End If
+
+
+        For i = 1 To 5
+            isIcludedInt = Array.IndexOf(hostNumberArray, i)
+            If isIcludedInt < 0 Then
+                hostNumberArray(3) = i
+                Exit For
+            End If
+        Next
+
+        For i = 1 To 5
+            isIcludedInt = Array.IndexOf(hostNumberArray, i)
+            If isIcludedInt < 0 Then
+                hostNumberArray(4) = i
+                Exit For
+            End If
+        Next
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim cdcInt As Integer
+        Dim hourInt As Integer
+        Dim inicTimeStr, hourStr, finTimeStr As String
+
+        'Shell("NET USE H: \\10.5.165.98\SharedDocuments Formula7 /USER:gtk\cvegabello")
+
+        'DateTimePicker1.Value = Now 'Today
+
+        cdcInt = returnCDC(DateTimePicker1.Value)
+        Label1.Text = cdcInt
+        'GroupBox1.Enabled = False
+        ProgressBar1.Maximum = 100
+        'okBtn.Visible = False
+        'cancelBtn.Visible = False
+        stopBtn.Visible = False
+        'Timer1.Enabled = True
+
+        hourInt = CInt(Format(TimeOfDay, "HH"))
+
+        If hourInt = 0 Then
+            hourInt = 23
+        Else
+            hourInt = hourInt - 1
+        End If
+
+        hourStr = Trim(Str(hourInt))
+
+        inicTimeStr = hourStr.PadLeft(2, "0") + ":" + "00" + ":" + "00"
+        finTimeStr = hourStr.PadLeft(2, "0") + ":" + "59" + ":" + "59"
+
+
+        Label2.Text = inicTimeStr
+        Label3.Text = finTimeStr
+
+
+    End Sub
+
+    
+
+    Private Sub DateTimePicker1_ValueChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DateTimePicker1.ValueChanged
+        Dim cdcInt As Integer
+        cdcInt = returnCDC(DateTimePicker1.Value)
+        Label1.Text = cdcInt
+    End Sub
+
+    Private Sub cancelBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cancelBtn.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub okBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles okBtn.Click
+        Dim errCode As Integer
+        Dim errorStr As String = ""
+        Dim cdcStr As String
+        Dim hostNumberArray(5) As Integer
+        Dim conStrinHostStr, inicTimeStr, finTimeStr As String
+        Dim substrings() As String
+
+        conStrinHostStr = GetSettingConfigHost(appName, "conStringHost", "").ToString()
+        substrings = conStrinHostStr.Split("|")
+
+        cdcStr = Trim(Label1.Text)
+
+        inicTimeStr = Trim(Label2.Text)
+        finTimeStr = Trim(Label3.Text)
+
+
+        getHostModeNew(substrings(3), substrings(0), substrings(4), hostNumberArray, errCode, errorStr)
+        'Utils.getHostMode("10.1.5.12", "prosys", "Numb3r1j0b", hostNumberArray, errCode, errorStr)
+
+
+        If errCode = 0 Then
+            'MsgBox(hostNumberArray(2))
+
+            Select Case hostNumberArray(2)
+
+                Case 1
+                    Utils.auto_GSRV_TMIR(substrings(1), substrings(0), substrings(2), cdcStr, inicTimeStr, finTimeStr, errCode, errorStr)
+                    'Utils.auto_IVAL_IPS_gxtmir("10.1.5.10", "prosys", "Numb3r1j0b", cdcStr, errCode, errorStr)
+                    If errCode <> 0 Then
+                        MsgBox("Error: " & errorStr)
+                    End If
+
+                Case 2
+
+                    Utils.auto_GSRV_TMIR(substrings(3), substrings(0), substrings(4), cdcStr, inicTimeStr, finTimeStr, errCode, errorStr)
+                    'Utils.auto_IVAL_IPS_gxtmir("10.1.5.11", "prosys", "Numb3r1j0b", cdcStr, errCode, errorStr)
+                    If errCode <> 0 Then
+                        MsgBox("Error: " & errorStr)
+                    End If
+
+
+                Case 3
+                    Utils.auto_GSRV_TMIR(substrings(5), substrings(0), substrings(6), cdcStr, inicTimeStr, finTimeStr, errCode, errorStr)
+                    If errCode <> 0 Then
+                        MsgBox("Error: " & errorStr)
+                    End If
+
+
+                Case 4
+                    Utils.auto_GSRV_TMIR(substrings(7), substrings(0), substrings(8), cdcStr, inicTimeStr, finTimeStr, errCode, errorStr)
+                    If errCode <> 0 Then
+                        MsgBox("Error: " & errorStr)
+                    End If
+
+
+                Case 5
+                    Utils.auto_GSRV_TMIR(substrings(9), substrings(0), substrings(10), cdcStr, inicTimeStr, finTimeStr, errCode, errorStr)
+                    If errCode <> 0 Then
+                        MsgBox("Error: " & errorStr)
+                    End If
+
+
+
+            End Select
+
+        Else
+
+            MsgBox(errorStr, MsgBoxStyle.Critical, "GetHostMode Error")
+
+
+        End If
+
+        MsgBox("Done", MsgBoxStyle.Information, "DONE")
+
+    End Sub
+
+End Class
